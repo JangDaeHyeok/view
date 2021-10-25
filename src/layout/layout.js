5d3c565813727d89a59890ca81b749a6a1669e79
choimiae <chlaldo1220@gmail.com>
2021-10-25 14:12:40 +0900

    react view 추가

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,30 +1,11 @@
-import React, {createContext, useEffect, useState} from "react";
+import React from "react";
 import Routers from "../component/router";
-import axios from "axios";
 
 function Layout() {
-
-    const UserContext = createContext('');
-    const [session, setSession] = useState();
-
-    useEffect(() => {
-        axios({
-            method: 'post',
-            url: '/user/checkSession',
-            headers: {'Content-Type': 'application/json', 'X-AUTH-TOKEN': session},
-        }).then(Response => {
-
-            console.log(Response)
-        }).catch((error) => {
-        });
-    }, [1]);
-
     return (
         <>
-            <UserContext.Provider value={session}>
-                {/* 라우터 - url, 컴포넌트 세팅 */}
-                <Routers/>
-            </UserContext.Provider>
+            {/* 라우터 - url, 컴포넌트 세팅 */}
+            <Routers/>
         </>
     )
 }
